31d99fe289db1225fa75c9bcc1ee01ac877d7269
mozahid <ce.muzahid@gmail.com>
2023-11-22 09:04:38 +0600

    added motion fadeIn animation

--- src/container/portfolio/portfolioSection.jsx
+++ src/container/portfolio/portfolioSection.jsx
@@ -1,4 +1,6 @@
 import Portfolio from './singlePortfolio'
+import {motion} from 'framer-motion'
+import {fadeIn} from '../../utils/variants'
 
 const array = [1, 2, 3]
 
@@ -6,16 +8,26 @@ const PortfolioSection = () => {
   return (
     <section className="">
       <div className="container px-5 py-24 mx-auto">
-        <div className="text-center mb-20">
+        <motion.div
+          variants={fadeIn('up', 0.2)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.7 }}
+          className="text-center mb-20">
           <h2 className="sm:text-5xl text-2xl font-medium title-font mb-4 uppercase">
             My Portfolio
           </h2>
-        </div>
-        <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+        </motion.div>
+        <motion.div
+          variants={fadeIn('right', 0.3)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.7 }}
+          className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
           {array.map((item, index) => (
             <Portfolio key={index} item={item} />
           ))}
-        </div>
+        </motion.div>
         <button className="btn btn-outline flex-center mt-16 mx-auto">
           Show More
         </button>
